bc8da137058135a53931e53d4b62473f2b8e52ac
quentin784487 <quentin784487@gmail.com>
2026-01-06 11:09:55 +0200

    initial_commit

---
+++ src/test/java/com/springboot/assetmanager/AssetmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.assetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
