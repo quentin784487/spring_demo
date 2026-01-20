33865c6a15ca37c4de6bf32c06f9cc4c52bba623
quentin784487 <quentin784487@gmail.com>
2026-01-20 12:27:19 +0200

    latest_changes

---
+++ data_tool/SQLGenerator/SQLGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+string folderPath = @"C:\temp\data";
+
+string[] files = Directory.GetFiles(folderPath);
+
+string sql = "INSERT INTO publishers (name) VALUES ";
+
+foreach (string file in Directory.EnumerateFiles(folderPath))
+{
+    foreach (string line in File.ReadLines(file))
+    {
+        //string test = "Quentin's";
+        if (line == "Starlight Software")
+        {
+            
+        }
+
+        string escapedLine = Regex.Replace(line, "'", "''");
+        escapedLine = Regex.Replace(escapedLine, "’", "''");
+        sql += "('" + escapedLine + "'), ";
+    }
+}    
+
+Console.Write(sql);
+
